ed1ed1e8b9ac28e492fdedd2f3b56163e0a99b09
kbko1234oos <kyungbong135@gmail.com>
2025-02-20 15:19:19 -0800

    Changed script to take both CSV and XSLX w pandas

--- Part1/combine_coords.py
+++ Part1/combine_coords.py
@@ -1,43 +1,41 @@
 import sys
+import pandas as pd
 
+def parse_dataframe(df, coord_mapping):
+    for _, row in df.iterrows():
+        protein_id, start, end, domain = row
+        unique_domain = f"{protein_id},{domain}"
 
-def parse_line(line, coord_mapping):
-    if line.strip():
-        # print(line)
-        protein_id, start, end, domain = line.split(',')
-        unique_domain = protein_id + ',' + domain
-
-        # New Coordinate + domain
         if unique_domain not in coord_mapping:
-            coord_mapping[protein_id + ',' + domain] = [start, end]
-        # Old Coordinate, need to see if replacement necessary
+            coord_mapping[unique_domain] = [start, end]
         else:
             if start < coord_mapping[unique_domain][0]:
                 coord_mapping[unique_domain][0] = start
             if end > coord_mapping[unique_domain][1]:
                 coord_mapping[unique_domain][1] = end
 
-
-def combine_coords(coordinates, output_file):
+def combine_coords(inputfile, outputfile):
+    # Determine file type and read accordingly
+    if inputfile.endswith('.csv'):
+        df = pd.read_csv(inputfile, header=None, names=['protein_id', 'start', 'end', 'domain'])
+    elif inputfile.endswith('.xlsx'):
+        df = pd.read_excel(inputfile, header=None, names=['protein_id', 'start', 'end', 'domain'])
+    else:
+        raise ValueError("Unsupported file format. Please use CSV or XLSX.")
 
     coord_mapping = {}
-
-    # Read coordinates file
-    with open(coordinates, 'r') as mapping:
-        coord_lines = mapping.readlines()
-        for line in coord_lines:
-            parse_line(line, coord_mapping)
+    parse_dataframe(df, coord_mapping)
 
     output_lines = []
-    # print(coord_mapping)
     for key, value in coord_mapping.items():
         protein_id, domain = key.split(',')
-        output_lines.append(protein_id + ',' + value[0] + ',' + value[1] + ',' + domain)
+        output_lines.append(f"{protein_id},{value[0]},{value[1]},{domain}\n")
 
-    with open(output_file, 'w') as output:
+    with open(outputfile, 'w') as output:
         output.writelines(output_lines)
 
+# Usage
+input_file = sys.argv[1]  # or 'your_input_file.csv'
+output_file = sys.argv[2]
+combine_coords(input_file, output_file)
 
-combine_coords(sys.argv[1], sys.argv[2])
-
-    

9a6833591fe806166224bacb422700300687b9b6
Bálint Szigeti Csúcs <166773036+bszigeticsucs@users.noreply.github.com>
2025-02-26 23:59:50 -0800

    Add test_simplify_headers.py

---
+++ Part1/test_simplify_headers.py
@@ -0,0 +1,13 @@
+import sys
+
+def test():
+    correct_file = open("./Dandie_file_to_test_on.fasta", "r")
+    output_file = open("simplified.fasta", "r")
+    correct_lines = correct_file.readlines()
+    output_lines = output_file.readlines()
+    #for i in range(len(output_lines) - 1):
+    for i in range(3298):
+        assert correct_lines[i] == output_lines[i]
+
+test()
+

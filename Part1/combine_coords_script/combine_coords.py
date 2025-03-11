fde01e4af639453006a38be5eb20a2d118cbae1d
Balint Szigeti Csucs <bszigeticsucs@ucdavis.edu>
2025-03-11 00:33:44 -0700

    Organize files, add test case and test script for combine_coords.py.

--- Part1/combine_coords_script/combine_coords.py
+++ Part1/combine_coords_script/combine_coords.py
@@ -2,14 +2,28 @@ import sys
 import csv
 
 def parse_data(data, coord_mapping):
+    current_unique_domain = "a"
     for row in data:
         protein_id, start, end, domain = row
         start, end = int(start), int(end)  # Convert to integers
         unique_domain = "{},{}".format(protein_id, domain)
 
+        if ((unique_domain + current_unique_domain[-1]) == current_unique_domain):
+            unique_domain = current_unique_domain
+
         if unique_domain not in coord_mapping:
             coord_mapping[unique_domain] = [start, end]
+            current_unique_domain = unique_domain
         else:
+            if current_unique_domain != unique_domain:
+                last_char = unique_domain[-1]
+                try:
+                    last_char = int(last_char)
+                    unique_domain.append(str(int(last_char) + 1))
+                except:
+                     unique_domain += "2"
+                current_unique_domain = unique_domain
+                coord_mapping[unique_domain] = [start, end]
             if start < coord_mapping[unique_domain][0]:
                 coord_mapping[unique_domain][0] = start
             if end > coord_mapping[unique_domain][1]:
@@ -31,7 +45,7 @@ def combine_coords(inputfile, outputfile):
         parse_data(reader, coord_mapping)
 
     with open(outputfile, 'w') as outfile:
-        writer = csv.writer(outfile)
+        writer = csv.writer(outfile, lineterminator = "\n")
         for key, value in coord_mapping.items():
             protein_id, domain = key.split(',')
             writer.writerow([protein_id, value[0], value[1], domain])

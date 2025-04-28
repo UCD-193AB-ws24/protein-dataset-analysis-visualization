aa02148369c01f06564942fb86359b47880bb2c2
Vishal Koppuru <vishalksai@gmail.com>
2025-04-27 18:41:17 -0700

    change colors of arrows for better visibility

--- Part2/Part2_Frontend/src/lib/Chart.svelte
+++ Part2/Part2_Frontend/src/lib/Chart.svelte
@@ -128,7 +128,20 @@
       return size > 1;
     });
 
-    const colorScale = d3.scaleOrdinal([...d3.schemeSet3]).domain(colorRoots);
+    const customColors = [
+      "#1f77b4",
+      "#ff7f0e",
+      "#2ca02c",
+      "#d62728",
+      "#9467bd",
+      "#8c564b",
+      "#e377c2",
+      "#7f7f7f",
+      "#bcbd22",
+      "#17becf"
+    ];
+    const colorScale = d3.scaleOrdinal(customColors).domain(colorRoots);
+    // const colorScale = d3.scaleOrdinal([...d3.schemeSet3]).domain(colorRoots);
     // Gray-out CCs not associated with colorRoots
     const nodeColor = new Map(
       nodes.map((n) => {

b2687ed81de9c1de29b5776cc0ebd57d8dd51c31
Gurqu1 <gurpreetgs19881@gmail.com>
2025-05-07 21:23:50 -0700

    removed authority section from userManager.ts for potential fix for CORS

--- Part2/Part2_Frontend/src/lib/auth/userManager.ts
+++ Part2/Part2_Frontend/src/lib/auth/userManager.ts
@@ -3,7 +3,6 @@ import { UserManager } from "oidc-client-ts";
 const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com";
 
 const cognitoAuthConfig = {
-    authority: cognitoDomain,
     client_id: "6s0tgt4tnp6s02o1j8tmhgqnem",
     redirect_uri: "https://d37wdymfu5cgqx.cloudfront.net/callback",
     response_type: "code",

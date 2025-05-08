96cc943f5639b3e0c3de3740a741f5fc3440826e
Vishal Koppuru <vishalksai@gmail.com>
2025-05-07 19:29:49 -0700

    fix cognito settings, edit yml to trigger redeploy

--- Part2/Part2_Frontend/src/lib/auth/userManager.ts
+++ Part2/Part2_Frontend/src/lib/auth/userManager.ts
@@ -1,9 +1,9 @@
 import { UserManager } from "oidc-client-ts";
 
 const cognitoAuthConfig = {
-    authority: "https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Bep0PJNNp",
+    authority: "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com/",
     client_id: "6s0tgt4tnp6s02o1j8tmhgqnem",
-    redirect_uri: "http://localhost:5173/callback",
+    redirect_uri: "https://d37wdymfu5cgqx.cloudfront.net/callback",
     response_type: "code",
     scope: "email openid phone"
 };
@@ -15,8 +15,8 @@ export const userManager = new UserManager({
 
 export async function signOutRedirect () {
     const clientId = "6s0tgt4tnp6s02o1j8tmhgqnem";
-    const logoutUri = "http://localhost:5173";
-    const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com";
+    const logoutUri = "https://d37wdymfu5cgqx.cloudfront.net";
+    const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com/";
     await userManager.removeUser();
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
 };

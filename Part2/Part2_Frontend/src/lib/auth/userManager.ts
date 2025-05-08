29d27553f058ba45036e497709b37bdf05bd6bff
Gurqu1 <gurpreetgs19881@gmail.com>
2025-05-07 21:15:57 -0700

    added meta data to userManager for potential fix to CORS

--- Part2/Part2_Frontend/src/lib/auth/userManager.ts
+++ Part2/Part2_Frontend/src/lib/auth/userManager.ts
@@ -1,12 +1,22 @@
 import { UserManager } from "oidc-client-ts";
 
+const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com";
+
 const cognitoAuthConfig = {
-    authority: "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com/",
+    authority: cognitoDomain,
     client_id: "6s0tgt4tnp6s02o1j8tmhgqnem",
     redirect_uri: "https://d37wdymfu5cgqx.cloudfront.net/callback",
     response_type: "code",
-    scope: "email openid phone"
-};
+    scope: "email openid phone",
+    metadata: {
+      issuer: cognitoDomain,
+      authorization_endpoint: `${cognitoDomain}/oauth2/authorize`,
+      token_endpoint: `${cognitoDomain}/oauth2/token`,
+      userinfo_endpoint: `${cognitoDomain}/oauth2/userInfo`,
+      end_session_endpoint: `${cognitoDomain}/logout`,
+      jwks_uri: `https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Bep0PJNNp/.well-known/jwks.json`, // Replace with your actual pool ID
+    }
+  };
 
 // create a UserManager instance
 export const userManager = new UserManager({

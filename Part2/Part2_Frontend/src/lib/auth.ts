eda4d45663fe1cd7e77a579dd460b2c8f40477a8
Gurqu1 <gurpreetgs19881@gmail.com>
2025-05-14 14:53:25 -0700

    added signout button

--- Part2/Part2_Frontend/src/lib/auth.ts
+++ Part2/Part2_Frontend/src/lib/auth.ts
@@ -7,3 +7,12 @@ export const oidcClient = new UserManager({
 	response_type: 'code',
 	scope: 'openid email phone'
 });
+
+
+export async function signOutRedirect () {
+    const clientId = "6s0tgt4tnp6s02o1j8tmhgqnem";
+    const logoutUri = "https://migrate-amplify.dsu4r7f5bomtn.amplifyapp.com";
+    const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com";
+    await oidcClient.removeUser();
+    window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
+};

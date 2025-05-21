cc931f4fbb3ee00b675e1dbddea0b34142928480
Vishal Koppuru <vishalksai@gmail.com>
2025-05-21 02:12:55 -0700

    Make auth.ts dynamic

--- Part2/Part2_Frontend/src/lib/auth.ts
+++ Part2/Part2_Frontend/src/lib/auth.ts
@@ -3,15 +3,14 @@ import { UserManager } from 'oidc-client-ts';
 export const oidcClient = new UserManager({
 	authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Bep0PJNNp',
 	client_id: '6s0tgt4tnp6s02o1j8tmhgqnem',
-	redirect_uri: 'https://main.dsu4r7f5bomtn.amplifyapp.com/callback',
+	redirect_uri: import.meta.env.VITE_REDIRECT_URI || 'http://localhost:5173/callback',
 	response_type: 'code',
 	scope: 'openid email phone'
 });
 
-
 export async function signOutRedirect () {
     const clientId = "6s0tgt4tnp6s02o1j8tmhgqnem";
-    const logoutUri = "https://main.dsu4r7f5bomtn.amplifyapp.com";
+    const logoutUri = import.meta.env.VITE_LOGOUT_URI || 'http://localhost:5173';
     const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com";
     await oidcClient.removeUser();
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;

150bc357ea0248d079891c649998779908e34b93
Vishal Koppuru <vishalksai@gmail.com>
2025-05-21 02:41:42 -0700

    attempt fix with env variables

--- Part2/Part2_Frontend/src/lib/auth.ts
+++ Part2/Part2_Frontend/src/lib/auth.ts
@@ -1,16 +1,17 @@
 import { UserManager } from 'oidc-client-ts';
+import { REDIRECT_URI, LOGOUT_URI } from './envs';
 
 export const oidcClient = new UserManager({
 	authority: 'https://cognito-idp.us-east-1.amazonaws.com/us-east-1_Bep0PJNNp',
 	client_id: '6s0tgt4tnp6s02o1j8tmhgqnem',
-	redirect_uri: import.meta.env.VITE_REDIRECT_URI || 'http://localhost:5173/callback',
+	redirect_uri: REDIRECT_URI,
 	response_type: 'code',
 	scope: 'openid email phone'
 });
 
 export async function signOutRedirect () {
     const clientId = "6s0tgt4tnp6s02o1j8tmhgqnem";
-    const logoutUri = import.meta.env.VITE_LOGOUT_URI || 'http://localhost:5173';
+    const logoutUri = LOGOUT_URI;
     const cognitoDomain = "https://us-east-1bep0pjnnp.auth.us-east-1.amazoncognito.com";
     await oidcClient.removeUser();
     window.location.href = `${cognitoDomain}/logout?client_id=${clientId}&logout_uri=${encodeURIComponent(logoutUri)}`;
